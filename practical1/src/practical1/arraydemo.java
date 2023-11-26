646017e7064c1881f78cd5c7d5372bd8fcdd8ffa
ADMIN <ADMIN@DESKTOP-P1397RD>
2023-11-26 18:37:15 +0530

    This code is for the ARRAY in Java Language

---
+++ practical1/src/practical1/arraydemo.java
@@ -0,0 +1,21 @@
+package practical1;
+
+public class arraydemo 
+{
+	public static void main(String[] args) 
+	{
+		int[] anARRAY;
+		   anARRAY = new int[5];
+	        anARRAY[0] = 100;
+	         anARRAY[1] = 200;
+	          anARRAY[2] = 300;
+	           anARRAY[3] = 400;
+	            anARRAY[4] = 500;
+		        
+	            System.out.println("Element at index 0 is "+ anARRAY[0]);
+	             System.out.println("Element at index 1 is "+ anARRAY[1]);
+ 	              System.out.println("Element at index 2 is "+ anARRAY[2]);
+            	   System.out.println("Element at index 3 is "+ anARRAY[3]);
+	                System.out.println("Element at index 4 is "+ anARRAY[4]);
+	}
+}
